3d056608b8487f243604e8221f086c0d7f546c9d
rteliz <9rteliz@stpatrick.org>
2017-02-01 14:07:20 -0600

    started degree

---
+++ Degree-master/degree.js
@@ -0,0 +1,25 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [1,1,0,1,0],
+    [1,0,1,0,0],
+    [0,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,0,1,0]
+]);
+//Code here
+function degree(mtrx,v){
+  let sum = 0;
+  let degreelist = 
+
+}
+
+function degreecentrality(){
+
+}
+
+//Challenge
+
+//Run test code with ctrl+shift+p  then type in "script"
+console.log(degree(A,3));
+console.log(degreecentrality(A,2));
