ac0e3869e613dc8030e273fee8089365d55ef17d
rteliz <9rteliz@stpatrick.org>
2017-02-02 13:57:55 -0600

    continued with degree

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# degree
+# Degree
---
+++ degree.js
@@ -0,0 +1,27 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [1,1,0,1,0],
+    [1,0,1,0,0],
+    [0,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,0,1,0]
+]);
+//Code here
+function degree(mtrx,v){
+  let sum = 0;
+  let degreelist = mtrx(v-1)
+
+  for(let );
+
+}
+
+function degreecentrality(){
+
+}
+
+//Challenge
+
+//Run test code with ctrl+shift+p  then type in "script"
+console.log(degree(A,3));
+console.log(degreecentrality(A,2));
